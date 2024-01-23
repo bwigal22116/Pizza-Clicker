108d4167eece775a44c273b66f2bf61e0d0aab89
Bearcat <bwigal2@nastudents.org>
2024-01-23 13:55:14 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 var pizza = 0
+localStorage.setItem("Pizza", pizza);
+if (pizza == 0) {
+var pizza = 0
+} else {
+   // code is executed
+}
 function ChangeClicksBy1() {
   let pizza = pizza + 1;
 }
+document.getElementById("myText").innerHTML = pizza;
